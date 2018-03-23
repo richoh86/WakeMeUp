48470cf8e485a2dde8640d330c79737bf3069928
ywangnon <ywangnon123@gmail.com>
2018-03-23 22:50:12 +0900

    move to dev

---
+++ WakeMeUp/WakeMeUp/Yoo/SettingCell.swift
@@ -0,0 +1,24 @@
+//
+//  SettingCell.swift
+//  WakeMeUp
+//
+//  Created by Hansub Yoo on 2018. 3. 23..
+//  Copyright © 2018년 richard oh. All rights reserved.
+//
+
+import UIKit
+
+class SettingCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        
+    }
+
+}
